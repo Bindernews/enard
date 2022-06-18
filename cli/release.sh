7b95023a2cced2fc5dac32940a2ad2920d74a4e4
Andrew Heintz <bindernews@gmail.com>
2022-06-18 15:41:41 -0400

    Another attempt at automatic releases

---
+++ cli/release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Github actions release script.
+# Assumes GITHUB_TOKEN and OS_NAME are set as 
+set -euo pipefail
+NAME="enard-cli"
+# Get the release tag from github ref
+tag="${GITHUB_REF#refs/tags/}"
+# Strip the version from the OS name
+os_name="${OS_NAME%-latest}"
+# Full name of the zip file
+zip_name="${NAME}_${tag}_${os_name}.zip"
+# Determine the executable file name and extension
+exe_file="target/release/${NAME}"
+if [ "$os_name" == "windows" ]
+  exe_file="${exe_file}.exe"
+fi
+# Build, zip, and upload to the tagged release
+cargo build --release
+zip ${zip_name} ${exe_file}
+gh release upload "${tag}" "${zip_name}" --clobber
