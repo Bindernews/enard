072271eebc30c12f845c7bc91ce489e92ed61022
Andrew Heintz <bindernews@gmail.com>
2022-06-18 16:36:53 -0400

    Fixing release.sh AGAIN and list MSRV in README.

--- cli/release.sh
+++ cli/release.sh
@@ -5,7 +5,21 @@
 set -euo pipefail
 NAME="enard-cli"
 # Determine if we're on windows
-IS_WIN=$([[ "$OS_NAME" =~ "windows-" ]] || "false")
+case "${OS_NAME}" in
+  windows-*)
+    platform="windows"
+    bin_suffix=".exe"
+    ;;
+  ubuntu-* | macos-*)
+    platform="unix"
+    bin_suffix=""
+    ;;
+  *)
+    echo "ERROR: unknown OS ${OS_NAME}" >&2
+    exit 1
+    ;;
+esac
+
 # Get the release tag from github ref
 tag="${GITHUB_REF#refs/tags/}"
 # Strip the version from the OS name
@@ -13,16 +27,15 @@ os_name="${OS_NAME%-latest}"
 # Full name of the zip file
 zip_name="${NAME}_${tag}_${os_name}.zip"
 # Determine the executable file name and extension
-exe_file="${NAME}"
-if $IS_WIN; then
-  exe_file="${exe_file}.exe"
-fi
+exe_file="${NAME}${bin_suffix}"
 # Build, zip, and upload to the tagged release
 cargo build --release
+# zip exe file
 mv "target/release/${exe_file}" ${exe_file}
-if $IS_WIN; then
+if [ "$platform" == "windows" ]; then
   7z a -tzip ${zip_name} ${exe_file}
 else
   zip ${zip_name} ${exe_file}
 fi
+# Release it
 gh release upload "${tag}" "${zip_name}" --clobber
